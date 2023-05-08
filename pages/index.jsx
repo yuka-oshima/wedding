01d0dd45cbdeacd400c62d2b3fd0f99d8b75685a
unknown <yuka.yamaguchi@firstloop-tech.com>
2023-05-08 23:35:29 +0900

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,27 @@
 import Head from "next/head";
-import { useLayoutEffect } from "react";
-import { useRouter } from "next/router";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
   const router = useRouter();
-  const uuid = router.query.id;
-//   useLayoutEffect(() => {
-//     if (uuid === undefined) return;
-//     router.replace(`/home/${uuid}`);
-//   }, []);
+  const uuid = "test";
+  //   useLayoutEffect(() => {
+  //     if (uuid === undefined) return;
+  //     router.replace(`/home/${uuid}`);
+  //   }, []);
 
   return (
-    <div >
+    <div>
       <Head>
         <meta name="description" content="home" />
       </Head>
-      <div>
-        <Link href={`/home/${uuid}`}><button>ホーム画面</button></Link>
-      </div>
+      <main>
+        <div>
+          <Link href={`/home/${uuid}`}>
+            <button>ホーム画面</button>
+          </Link>
+        </div>
+      </main>
     </div>
   );
 }
