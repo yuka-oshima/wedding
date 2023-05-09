1eeb4b76dcb26eb0bf620e7f6e604c975edd706a
Yuri <yuri.oda.lily@gmail.com>
2023-05-09 20:11:29 +0900

    connect MongoDB

--- pages/home/[id]/index.jsx
+++ pages/home/[id]/index.jsx
@@ -1,35 +1,78 @@
+import { MongoClient, ObjectId } from "mongodb";
 import React from "react";
 import Index from "../../../components/home/Index";
 
-
-export default ({post})=>{
+export default function UserDetails(props) {
   return (
     <>
-      <Index data={post}/>
+      <p>{props.userData.id}</p>
     </>
   );
 }
 
+// export default ({ post }) => {
+//   return (
+//     <>
+//       <Index data={post} />
+//     </>
+//   );
+// };
+
 export const getStaticPaths = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/user`)
-  const posts = await res.json() 
+  const client = await MongoClient.connect(process.env.MONGO_URI);
+  const db = client.db();
+  const userCollection = db.collection("users");
+  const users = await userCollection.find({}, { _id: 1 }).toArray();
+  client.close();
 
-  const paths = posts.map((post) => ({
-    params: {
-      id: post.id.toString(),
-    },
-  }))
-  return { paths, fallback: false }
-}
+  return {
+    fallback: "blocking",
+    paths: users.map((user) => ({
+      params: { id: user._id.toString() },
+    })),
+  };
 
+  // const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/user`);
+  // const posts = await res.json();
+  // const paths = posts.map((post) => ({
+  //   params: {
+  //     id: post.id.toString(),
+  //   },
+  // }));
+  // return { paths, fallback: false };
+};
 
-export const getStaticProps = async ({ params }) => {  
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/user/${params}`)
-  const post = await res.json()  
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+
+  const client = await MongoClient.connect(process.env.MONGO_URI);
+  const db = client.db();
+  const userCollection = db.collection("users");
+
+  const selectedUser = await userCollection.findOne({
+    _id: new ObjectId(id),
+  });
+
+  console.log(selectedUser);
+  client.close();
+
+  //6454d3020cfa5737e3472c87
 
   return {
     props: {
-      post
+      userData: {
+        id: selectedUser._id.toString(),
+      },
     },
-  }
-}
+  };
+
+  // const res = await fetch(
+  //   `${process.env.NEXT_PUBLIC_API_URL}/api/user/${params}`
+  // );
+  // const post = await res.json();
+  // return {
+  //   props: {
+  //     post,
+  //   },
+  // };
+};
